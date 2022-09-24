6619c0f563859b893ca2876082873b8815a232ec
Gloria Dukuzeyesu <gloriadukuzeyesu@glorias-mbp.uconnect.utah.edu>
2022-09-24 14:07:11 -0600

    Adding pure virtual function and Abstract class

--- Practices_C++ OOP/VirtualPureVirtualFunctions/VirtualPureVirtualFunctions/main.cpp
+++ Practices_C++ OOP/VirtualPureVirtualFunctions/VirtualPureVirtualFunctions/main.cpp
@@ -1,29 +1,33 @@
 //virtual functions
 // pure functions
+// abstract class is a class whihc has at least one virtual function
 
 
 #include <iostream>
 using namespace std;
 
 class Instrument {
-private:
-    //member variables
-    
-public:
-    virtual void MakeSound(){
-        cout << "Instrument is playing" << endl;
-    }
-};
 
+public:
+    virtual void MakeSound() = 0;  // pure virtual function
+    // this means that every function that will inherit from base class ( class instrumetn)
+    // will have to provide its own derived function makesound()
+    //
+};
 //derived class
 class Guitar: public Instrument {
 public:
     void MakeSound(){
         cout << "Guitar is playing" << endl;
     }
-    
 };
 
+class Piano : public Instrument {
+public:
+    void MakeSound(){
+        cout << "Piano is playing" << endl;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     /*
@@ -34,5 +38,15 @@ int main(int argc, const char * argv[]) {
     Instrument* I1 = new Guitar();
     I1->MakeSound();
     
+    //pure virtual function implementation
+    Instrument* I2 = new Piano();
+    I2->MakeSound();
+  
+    cout <<"--------------------implementation of polymorphism-------------------- " << endl;
+    // make an array of Instrument class pointer
+    Instrument* Instruments [2] = {I1, I2};
+    for(int i= 0; i< 2; i++){
+        Instruments[i] -> MakeSound();
+    }
     return 0;
 }
